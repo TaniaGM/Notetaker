e3c83de2561388eaa7704fb8b93ea2a1bede6db5
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-03-18 14:10:33 -0400

    cleaning file structures

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
-// Dependencies
-// =============================================================
+
+
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-// Sets up the Express App
-// =============================================================
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Routes
-// =============================================================
 
-// Basic route that sends the user to the index page
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-// Basic route that sends the user to the notes page
+
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// Route that sends the user the db.json file
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "db.json"));
 });
 
-// Takes a JSON input with keys "title" and "text" and adds a new note object with that message to the db.json file
 app.post("/api/notes", function(req, res) {
     fs.readFile(path.join(__dirname, "db.json"), "utf8", function(error, response) {
         if (error) {
@@ -54,7 +49,6 @@ app.post("/api/notes", function(req, res) {
     });
 });
 
-// Deletes the note object with requested id from the db.json file, returns the deleted note; if no such id exists returns false
 app.delete("/api/notes/:id", function(req, res) {
     const deleteId = req.params.id;
     fs.readFile("db.json", "utf8", function(error, response) {
@@ -63,9 +57,7 @@ app.delete("/api/notes/:id", function(req, res) {
         }
         let notes = JSON.parse(response);
         if (deleteId <= notes.length) {
-            // Method to remove an element from an array obtained from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
             res.json(notes.splice(deleteId-1,1));
-            // Reassign the ids of all notes
             for (let i=0; i<notes.length; i++) {
                 notes[i].id = i+1;
             }
@@ -82,7 +74,7 @@ app.delete("/api/notes/:id", function(req, res) {
 });
 
 // Starts the server to begin listening
-// =============================================================
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
